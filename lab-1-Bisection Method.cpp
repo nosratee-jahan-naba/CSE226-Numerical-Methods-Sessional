e5acf8db67ef2112ce802c9b759e18c5d56d8763
Nosratee Jahan Naba <187439614+nosratee-jahan-naba@users.noreply.github.com>
2025-07-07 09:30:45 +0600

    Update lab-1-Bisection Method.cpp

--- lab-1-Bisection Method.cpp
+++ lab-1-Bisection Method.cpp
@@ -1,62 +1,37 @@
-// Bisection Method/interval halving method/ binary search method / the dichotomy method.
 #include<bits/stdc++.h>
 using namespace std;
-#define tol 0.01
-
+#define tol 0.0001
 double f(double x)
 {
-    return x*x*x - x - 2;
+    return x*x*x - x -2;
 }
-
 void bisection(double a, double b)
 {
-    if (f(a) * f(b) >= 0)
-    {
-        cout << "Invalid interval" << endl;
+    if(f(a)*f(b) >= 0)
         return;
-    }
 
     double c;
-    while ((b-a) >= tol)    // so (b - a) < ep --> stop
+    int i=0;
+    while(i < 100)
     {
         c = (a+b)/2;
-
-        if (f(c) == 0.0)
+        if(f(c)== 0 || abs(f(c)) <= tol)
             break;
-
-        else if (f(c)*f(a) < 0)
-            b = c;
+        else if(f(a)*f(c)< 0)
+            b=c;
         else
-            a = c;
+            a=c;
+        i++;
     }
-    cout << "root = " << c;
+    if(i >= 100)
+        cout << "Not converged" << endl;
+    else
+        cout << c << endl;
 }
-
 int main()
 {
-    // double a =-200, b = 300;
-    int a,b;
+    double a, b;
     cin >> a >> b;
-
-    bisection(a, b);
-
+    bisection(a,b);
     return 0;
 }
-
-// Converged = Successfully found the root.
-// Not Converged = Did not find the root / method failed.
-
-
-// if f(x) is a continuous function on the interval [a, b] and f(a) and f(b) have opposite signs (i.e., f(a)⋅f(b)<0), then there is 
-// at least one root of the equation f(x)=0 in the interval (a,b).
-
-// Guaranteed Convergence: It always converges to a root 
-// if the function is continuous and f(a)⋅f(b) < 0.
-
-// What happens if f(a)⋅f(b) ≥ 0 ?
-// If f(a)⋅f(b) ≥ 0, the method cannot be applied because it indicates that either no root exists in the interval or there may be
-//  multiple roots or the root might not be in the interval.
-
-// How does the bisection method compare to other root-finding methods?
-// The bisection method is slower compared to methods like Newton's method or secant method, but it is more robust and simple to implement, especially for 
-// functions where derivatives are difficult to compute.
