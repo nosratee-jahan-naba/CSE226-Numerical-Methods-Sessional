5ecb9426ca7473517d0290dd31d29438e0f0b40d
Striver <187439614+nosratee-jahan-naba@users.noreply.github.com>
2025-07-20 21:11:18 +0600

    Update One-point-method.cpp

--- Lab-2/One-point-method.cpp
+++ Lab-2/One-point-method.cpp
@@ -1,52 +1,40 @@
 #include <bits/stdc++.h>
-#define tol 0.001
+#define tol 0.0001
 using namespace std;
 
-double f(double x) 
+double f(double x)
 {
-    return 3 * x  - cos(x) - 1;
+    return 3*x - cos(x) -1;
 }
-double g(double x) 
-{
-    return (1 + cos(x)) / 3;
-}
-void onepoint(double x0) 
-{
-    int i = 1;
-    double x = x0,xn; 
 
-    while (i <= 100)
+double g(double x)
+{
+    return (cos(x)+1)/3;
+}
+
+void onepoint(double xo)
+{
+    int i=0;
+    double x=xo,xn;
+    while(i<=100)
     {
         xn = g(x);
-        cout << xn << endl;
-        if (fabs(x - xn) < tol)
-            break;
-        
+        if(fabs(xn-x) < tol || xn == 0)
+        {
+            cout << "root = "<< xn << endl;
+            return;
+        }
+
         x = xn;
         i++;
     }
-    if (i > 100)
-        cout << "Did not converge within 100 iterations." << endl;
-    else
-        cout << "The value of the root is : " << x << endl;
+    cout << "Did not converge within 100 iterations." << endl;
 }
-int main() 
+
+
+int main()
 {
-    double x0; 
-    cin >> x0;
-    onepoint(x0);
+    double xo; cin >> xo;
+    onepoint(xo);
     return 0;
 }
-
-
-//------ why  (fabs(f(xn)) <= tol)   ?
-// Because in root-finding, we want f(x) = 0. But computers can’t always get exactly zero,
-//  so we accept a very small error (tol) as good enough.
-
-
-//------ Why use max iterations (n)?
-// To stop the program if it runs too long without finding the root.
-// To avoid infinite loops when the method doesn’t converge.
-// To save computer time and resources.
-// To know if the method is not working properly or the initial guess is bad.
-// To make the program safe and reliable.
